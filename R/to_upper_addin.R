1baba9e1448693e1605ea0546bbe5803ea8edcc2
Hayden Rabel <hayden.rabel@gmail.com>
2019-08-20 09:45:35 +1200

    add touppercase

---
+++ R/to_upper_addin.R
@@ -0,0 +1,12 @@
+#' Transform selected text to uppercase
+#'
+#' \code{to_upper_addin} uses the rstudioapi to get the selected text
+#' then replace it with the uppercase version
+#'
+to_upper_addin <- function() {
+  ctx <- rstudioapi::getActiveDocumentContext()
+  STR <- toupper(ctx$selection[[1]]$text)
+  # ctx$contents
+  rstudioapi::insertText(location = ctx$selection[[1]]$range,
+                         text = STR)
+}
